b62528456958309488bf872cb044f3e8d52e1772
RobertoJudet <roberto@syneto.eu>
2020-11-19 10:44:03 +0200

    Homework for course12

---
+++ delivery_service.py
@@ -0,0 +1,17 @@
+# You have a company which transports people, electronics and food.
+# Transportation can be made with buses, cars, minivans, motorcycles and bicycles.
+# Make a class for each of this type with the following attributes
+#    - number of wheels
+#    - consumption
+#    - mileage
+#    - speed
+#    - type of transport: buses & cars transport people, minivans electronics and the rest food.
+#    - capacity
+# Make a class for the delivery company which has a list with all its vehicles
+#    - 2 buses, 4 cars, 1 minivan, 3 motorcycles and 6 bicycles
+#    - a function to compute how many people it can transport
+#    - a function to compute how many electronics it can transport
+#    - a function to compute how many food orders it can deliver from 9am to 6pm
+#        - bicycle takes 20min for an order, motorcycle takes 10min
+#        - drivers need to take 10min break after 1h and have 1h lunch break
+
---
+++ electricity_bill.py
@@ -0,0 +1,12 @@
+# An electricity company charges the following rates to its users:
+# For the first 100 units – 0.06$ per unit
+# For the first 200 units – 0.08$ per unit
+# For the first 300 units – 0.09$ per unit
+# After 300 units - 0.1$ per unit
+#
+# Every client pays 10$ for maintenance/support
+# If the total amount is more than 400 units then an additional surcharge of 5% is added
+#
+# Write a function and some tests which reads from the electricity_bill.json
+# and can compute the bill for any given user.
+
