18ea908229ca9e313a123fbde0ce17473dc3bac9
RobertoJudet <roberto@syneto.eu>
2020-12-02 21:58:26 +0200

    homework14

---
+++ 1_new_homework.py
@@ -0,0 +1,14 @@
+# Make 2 tests for each consumption class
+# message `... has to pay ...` should be returned by a method in consumption class (make some tests for it)
+
+# make a public voting system
+# there are 100 users who can vote (name0, name1, ..., name 99)
+# there are 4 parties which can be voted (party0, party1, party2, party3)
+# register the votes in a file, w/o the user who voted
+# register the users who voted in a file so a user can't vote 2 times
+
+# make a voting system for a company
+# there are 30 shareholders with 1%, 10 shareholders with 3%, 1 with 10% and 1 with 20%
+# they vote on 3 decisions: increase investments, cut salaries & expand to Asia
+# each shareholders can vote YES/NO on every decision or not vote
+# a decision is taken if at least 30% shares have voted and if 51% of the votes are YES
