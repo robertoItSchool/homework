48d29de065e8cacabbb9713cd8198de3808e5d6d
RobertoJudet <roberto@syneto.eu>
2020-12-07 20:50:06 +0200

    Add homework

---
+++ 1_homework_for_voting.py
@@ -0,0 +1,8 @@
+# make 2 tests for User.has_user_already_voted()
+# make a test for User.register_the_vote()
+# make a test for User.register_to_users_who_voted()
+
+# make a function which computes the election score
+
+# we can see which user voted for which party because they are in the same order
+# find a way to hide this correlation
