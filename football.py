d339ca8f675ef59892f955fa39d87e7c3b2faab4
RobertoJudet <roberto@syneto.eu>
2020-12-10 15:21:40 +0200

    Homework for Monday

---
+++ football.py
@@ -0,0 +1,8 @@
+# Define a class for a football team, only the starting eleven (no substitutes/reserves)
+# it can contain 3-5 defenders, 3-5 midfield players and 1-3 strikers; it has to have 11 players in total, 1 goalkeeper
+# all players have stats from 1-10 (1 bad, 10 perfect)
+# goalkeepers: communication, aerial reach, reflexes, passing, concentration, stamina, strength
+# defenders: communication, tackling, heading, passing, concentration, stamina, strength
+# midfield players: tackling, dribbling, heading, passing, concentration, stamina, strength
+# strikers: dribbling, finishing, heading, passing, composure, stamina, strength
+# create 3 teams of players, one with good players, one with medium players & one with weak players
