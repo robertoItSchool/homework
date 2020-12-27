df2a3a6fd51edcdd9f7973a9e0d254723b91c404
Roberto Judet <roberto.judet@itschool.ro>
2020-12-27 14:41:08 +0200

    Add OOP homework exercises

---
+++ flower_shop.py
@@ -0,0 +1,6 @@
+# Create a flower shop application which deals in flower objects 
+# and use those flower objects in a bouquet object which can then be sold. 
+# Keep track of the number of objects and when you may need to order more (keep them in a file) 
+# Allow the user to give commands through the terminal (use sys.argv to read the commands)
+# Make unit tests
+
---
+++ reservation.py
@@ -0,0 +1,8 @@
+# Create a reservation system which books airline seats and hotel rooms. 
+# It charges various rates for particular sections of the plane or hotel. 
+# Example, first class is going to cost more than coach. 
+# Hotel rooms have penthouse suites which cost more. 
+# Keep track of when rooms/seats will be available and can be scheduled (save them in a file)
+# Create commands which can be given through the terminal (use sys.argv to read the params)
+# Make unit tests for functions
+
