bbde3f117ada259e08d87f25a53aa921e1ccfe88
Roberto Judet <roberto.judet@itschool.ro>
2021-01-07 11:04:33 +0200

    Add weather exercise

---
+++ weather_reports.py
@@ -0,0 +1,8 @@
+# create a weather report for the current day (& hour) for a city (Bucharest, Timisoara, Oradea, Suceava, Brasov)
+# you can use the API from https://www.weatherbit.io/
+# url for current day: https://api.weatherbit.io/v2.0/current
+# params: city and key; for key you can use mine 1ab5e9499298416b97674b2facdaeb1c or make a free account on the site
+# save the weather report in a file & make a class which can reconstruct the object from the file
+# every weather report is appended to its city, we never delete a previous day
+# weather report should contain a description of the weather, temperature, clouds metric & sunset time
+
