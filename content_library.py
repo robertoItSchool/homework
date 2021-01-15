62175f5bf891772850b27fcbb3b0114ffa5047e6
Roberto Judet <roberto.judet@itschool.ro>
2021-01-15 14:47:19 +0200

    Add spec for homework

---
+++ content_library.py
@@ -0,0 +1,6 @@
+# Create an app to keep track of the entertainment/content you consume
+# We should be able to create multiple profiles(users) 
+# Each user can track the progress of games, films, tv shows & books 
+# Each user can give ratings
+# All these things are saved in a file as JSON
+
